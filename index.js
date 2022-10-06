4e50a3ddda4ef697114e6727eda7bfcb7dd2d084
Robbie Leib <robert.leib@gmail.com>
2022-10-06 12:34:24 -0600

    log out labels

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const github = require('@actions/github')
 const run = async () => {
   try {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+    const githubToken = core.getInput('github_token')
+    if (!githubToken) {
+      throw Error(`input 'github_token' is required`)
+    }
+
+    const existingLabels = core
+      .getInput('labels')
+      .split('\n')
+      .filter(l => l !== '')
+
     console.log(`The event payload: ${payload}`)
+    console.log('The repo labels:', existingLabels)
+
   } catch (error) {
     console.log('Error:', error)
-    core.error(error);
-    core.setFailed(error);
+    core.error(error)
+    core.setFailed(error)
   }
 }
 
