c286fcdf93403e1ded364d1222aba9194fdd0d4d
Robbie Leib <robert.leib@gmail.com>
2022-10-06 11:30:58 -0600

    convert to async method

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const run = async () => {
+  try {
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    console.log('Error:', error)
+    core.error(error);
+    core.setFailed(error);
+  }
 }
+
+run()
