adee534cdb74e1cea048e11a6063f1c5bf7c5894
Robbie Leib <robert.leib@gmail.com>
2022-10-06 19:06:09 -0600

    add labels based on existing labels

--- index.js
+++ index.js
@@ -4,10 +4,33 @@ const github = require('@actions/github')
 const run = async () => {
   try {
     const githubToken = core.getInput('github_token')
+    const approveViaComment = core.getInput('approve_via_comment') == '' ? true : true
+
+    if (!approveViaComment && github.context.payload?.review?.state == 'commented') {
+      return
+    }
+
     if (!githubToken) {
       throw Error(`input 'github_token' is required`)
     }
     const client = github.getOctokit(githubToken)
+
+    const action = github.context.payload.action
+    let delta = 0
+    if (action == 'dimissed') {
+      delta = -1
+    }
+    if (action == 'submitted' && github.context.payload?.review?.state == 'approved') {
+      delta = +1
+    }
+
+    if (approveViaComment && github.context.payload?.review?.state == 'commented') {
+      let body = github.context.payload?.review?.body
+      if(body !== '' && body.includes('dude')) {
+        delta = +1
+      }
+    }
+    if (delta == 0) { return }
 
     const pr = await client.rest.issues.get({
       owner: github.context.payload.repository.owner.login,
@@ -26,17 +49,61 @@ const run = async () => {
       issue_number: github.context.payload.pull_request.number
     })
 
-    existingLabels = labels
+    const existingPlusLabels = labels
       .data
       .map(label => label.name)
-      .filter(l => l !== '')
+      .filter(l => l == '+1' || l == '+2')
 
-
-
-    // const existingLabels = pr.data.labels.map(label => label.name).filter(l => l !== '')
-
+    const existingLabels = labels
+      .data
+      .map(label => label.name)
+      .filter(l => l !== '' || l !== '+1' || l !== '+2')
 
     console.log('The repo labels:', existingLabels)
+
+    let currentPlusValue = 0
+    if (existingPlusLabels.includes("+1")) { currentPlusValue = 1 }
+    if (existingPlusLabels.includes("+2")) { currentPlusValue = 2 }
+
+    let newLabel
+    let newLabels = existingLabels
+
+    if (delta > 0) {
+      switch(currentPlusValue) {
+        case 2:
+          return
+        case 1:
+          newLabel = "+2"
+          break
+        case 0:
+          newLabel = "+1"
+          break
+        default:
+          return
+      }
+    } else {
+      switch(currentPlusValue) {
+        case 2:
+          newLabel = "+1"
+        case 1:
+          break
+        case 0:
+          return
+        default:
+          return
+      }
+    }
+
+    if (newLabel) {
+      newLabels.push(newLabel)
+    }
+
+    client.rest.issues.setLabels({
+      owner: github.context.payload.repository.owner.login,
+      repo: github.context.payload.repository.name,
+      issue_number: github.context.payload.pull_request.number,
+      labels: newLabels
+    })
 
   } catch (error) {
     console.log('Error:', error)
